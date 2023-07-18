a04fcf2dd98801d479bfd9fce9fcb93fd33faa8c
Patricia Sanchez <patsanchezmessa@gmail.com>
2023-07-18 19:45:56 -0300

    N04-intensivao

---
+++ script.js
@@ -0,0 +1,7 @@
+function enviar() {
+  let elemName = document.getElementById("nome");
+  let ElemEmail = document.getElementById("email");
+  let ElemComments = document.getElementById("comments");
+
+  console.log(elemName.value, ElemEmail.value, ElemComments.value);
+}
